af2775b4dcaa42d5df0b780da50fa2564281501a
chanhx <ichan.hocheung@gmail.com>
2022-05-14 22:20:21 +0800

    [sql] scan number

--- crates/sql/src/parser/lexer.rs
+++ crates/sql/src/parser/lexer.rs
@@ -14,10 +14,11 @@ impl<'a> Iterator for Lexer<'a> {
 
     fn next(&mut self) -> Option<Self::Item> {
         // consume whitespace
-        while let Some(_) = self.iter.next_if(|(_, c)| c.is_whitespace()) {}
+        self.iter_next_while(|c| c.is_whitespace());
 
         let token = match self.iter.peek() {
             Some((_, '\'')) => self.scan_string(),
+            Some((_, c)) if c.is_digit(10) => Ok(self.scan_number()),
             _ => Ok(None),
         };
 
@@ -38,6 +39,10 @@ impl<'a> Lexer<'a> {
             src,
             iter: src.char_indices().peekable(),
         }
+    }
+
+    fn iter_next_while(&mut self, func: impl Fn(&char) -> bool) {
+        while let Some(_) = self.iter.next_if(|(_, c)| func(c)) {}
     }
 
     fn scan_string(&mut self) -> Result<Option<Token<'a>>> {
@@ -67,6 +72,30 @@ impl<'a> Lexer<'a> {
             Details::NoClosingQuoteForString,
         ))
     }
+
+    fn scan_number(&mut self) -> Option<Token<'a>> {
+        let begin = self.iter.next_if(|&(_, c)| c.is_digit(10));
+        if begin.is_none() {
+            return None;
+        }
+
+        let begin = begin.unwrap().0;
+
+        self.iter_next_while(|c| c.is_digit(10));
+        self.iter.next_if(|&(_, c)| c == '.');
+        self.iter_next_while(|c| c.is_digit(10));
+        self.iter.next_if(|&(_, c)| c == 'e' || c == 'E');
+        self.iter.next_if(|&(_, c)| c == '+' || c == '-');
+        self.iter_next_while(|c| c.is_digit(10));
+
+        let end = self
+            .iter
+            .peek()
+            .map(|&(i, _)| i - 1)
+            .unwrap_or(self.src.len() - 1);
+
+        Some(Token::Number(&self.src[begin..=end]))
+    }
 }
 
 #[cfg(test)]
@@ -90,5 +119,18 @@ mod tests {
         let mut lexer = Lexer::new(input);
 
         assert!(matches!(lexer.next(), Some(Err(_))));
+    }
+
+    #[test]
+    fn scan_number() {
+        let nums = ["123.", "123.456e+789"];
+        let input = format!("  {}   {}", nums[0], nums[1]);
+
+        let mut lexer = Lexer::new(&input);
+
+        nums.iter().for_each(|&num| match lexer.next() {
+            Some(Ok(Token::Number(s))) => assert_eq!(s, num),
+            _ => unreachable!(),
+        });
     }
 }
